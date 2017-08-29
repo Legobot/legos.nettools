a4809bbf020adc622fd56dad4738096ef6f56210
zSec <31191533+z-Sec@users.noreply.github.com>
2017-08-29 19:12:39 +0200

    Adding DNS lookup for various records
    Implemented DNS lookup for A, AAAA, CNAME, MX, TXT records.

--- legos/tools/Dns.py
+++ legos/tools/Dns.py
@@ -1,16 +1,14 @@
-#!/usr/bin/env python
-
 import dns.resolver
 
 from Legobot.Utilities import Utilities as utils
 from legos.tools.Tool import ToolScheme
 
-__author__ = "Nitrax <nitrax@lokisec.fr>"
+__author__ = "zSec <b1337@zsec.de>"
 __copyright__ = "Copyright 2017, Legobot"
 
 
 class Dns(ToolScheme):
-    """This class retrieves the DNS record behind a given domain name.
+    """The DNS class allows a DNS lookup for various records.
     """
 
     def __init__(self, args):
@@ -18,7 +16,11 @@ class Dns(ToolScheme):
             super().__init__(args)
 
             self.fncs = {
+                'getA': self._getA,
+                'getAAAA': self._getAAAA,
+                'getCNAME': self._getCNAME,
                 'getMX': self._getMX,
+                'getTXT': self._getTXT
             }
 
     def run(self):
@@ -33,21 +35,92 @@ class Dns(ToolScheme):
                         results.append('Command unknown: ' + cmd)
             else:
                 results.append(self._getA())
+                results.append(self._getAAAA())
+                results.append(self._getCNAME())
+                results.append(self._getMX())
+                results.append(self._getTXT())
 
             return '\n'.join(results)
         else:
             return self.getHelp()
 
+    def _getA(self):
+        """Get the A record
+       Args:
+           self: self
+           
+       Returns:
+           str: A Record
+       """
+       	try:
+            data = dns.resolver.query(self.target, 'A')
+            results = []
+            for item in data:
+                results.append(str(item.exchange))
+            return ' | '.join(results)
+        except:
+            return 'Domain name invalid'
+
+    def _getAAAA(self):
+        """Get the AAAA record
+       Args:
+           self: self
+           
+       Returns:
+           str: AAAA record
+       """
+       	try:
+            data = dns.resolver.query(self.target, 'AAAA')
+            results = []
+            for item in data:
+                results.append(str(item.exchange))
+            return ' | '.join(results)
+        except:
+            return 'Domain name invalid'
+
+    def _getCNAME(self):
+        """Get the CNAME record
+       Args:
+           self: self
+           
+       Returns:
+           str: CNAME record
+       """
+       	try:
+            data = dns.resolver.query(self.target, 'CNAME')
+            results = []
+            for item in data:
+                results.append(str(item.exchange))
+            return ' | '.join(results)
+        except:
+            return 'Domain name invalid'
+
     def _getMX(self):
-        """Get MX records
+        """Get the MX record
+       Args:
+           self: self
+           
+       Returns:
+           str: MX record
+       """
+       	try:
+            data = dns.resolver.query(self.target, 'MX')
+            results = []
+            for item in data:
+                results.append(str(item.exchange))
+            return ' | '.join(results)
+        except:
+            return 'Domain name invalid'
 
-        Args:
-            self: self
-
-        Returns:
-            str: MX records
-        """
-        try:
+    def _getTXT(self):
+        """Get the TXT record
+       Args:
+           self: self
+           
+       Returns:
+           str: TXT record
+       """
+       	try:
             data = dns.resolver.query(self.target, 'MX')
             results = []
             for item in data:
@@ -57,4 +130,6 @@ class Dns(ToolScheme):
             return 'Domain name invalid'
 
     def getHelp(self):
-        return "!dns {--getMX} {target}"
+        return "!dns {--getA | --getAAAA |" \
+               "--getCNAME | --getMX |" \
+               "--getTXT} {target}"
